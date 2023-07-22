2f2945b59cbbfc7086ef62417d4c68af3b51c5a5
wolfgunblood <jalajdorai1@gmail.com>
2023-07-22 11:11:49 +0530

    Memoization All Construct

---
+++ memoization/all-construct.js
@@ -0,0 +1,36 @@
+const allConstruct = (target,wordBank) => {
+
+    if(target === "") return [[]];
+
+    const result = [];
+
+    for(let word of wordBank){
+
+        if(target.indexOf(word) === 0){
+            
+            const suffix = target.slice(word.length)
+            const suffixWays = allConstruct(suffix,wordBank)
+            const targetWays = suffixWays.map(way => [word,...way])
+            // console.log(targetWays)
+           
+            result.push(...targetWays);
+            
+        }
+    }
+
+    return result;
+
+}
+
+console.log(allConstruct("purple",["purp","p","ur","le","purpl"])) //2
+console.log(allConstruct("abcdef",["ab","abc","cd","def","abcd"])) //1
+console.log(allConstruct("skateboard",["bo","rd","ate","t","ska","sk","boar"])) //0
+console.log(allConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])) //4
+// console.log(allConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",[
+//     "e",
+//     "ee",
+//     "eee",
+//     "eeee",
+//     "eeeee",
+//     "eeeeee"
+// ]))
