72a53592368d329000c6becfb64528c6c42f638a
wolfgunblood <jalajdorai1@gmail.com>
2023-07-22 08:25:40 +0530

    Added comment for error in how sum

--- memoization/how-sum.js
+++ memoization/how-sum.js
@@ -27,8 +27,9 @@ console.log(howSum(7,[2,3])) //[3,2,2]
 console.log(howSum(7,[5,3,4,7])) // [4,3]
 console.log(howSum(7,[2,4])) // null
 console.log(howSum(8,[2,3,5])) // [2,2,2,2]
-console.log(howSum(300,[7,14])) 
+console.log(howSum(300,[7,14])) //null
 
+//Error for memo[targetSum] = []
 // [
 // 14, 7, 7, 7, 7, 7, 7, 7, 7, 7,
 // 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
