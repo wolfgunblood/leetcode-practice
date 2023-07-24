dd00a0e699f7092168fcfce88bf1b2294689dd2f
wolfgunblood <jalajdorai1@gmail.com>
2023-07-24 08:28:04 +0530

    33 Search in rotated sortes array

---
+++ BInary Search/33-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        int result = -1;
+
+        while(low<=high){
+
+            int mid = low + (high-low)/2;
+
+            if(nums[mid] == target) return mid;
+
+            if(nums[low] <= nums[mid])
+            {
+                if(nums[low] <= target && target <= nums[mid])
+                {
+                    high = mid-1;
+
+                }else{
+                    low = mid+1;
+                }
+            }else{
+                if(nums[high] >= target && target >=nums[mid] ){
+                    low = mid+1;
+                
+                }else{
+                    high = mid-1;
+                }
+            }
+
+        }
+
+        return -1;
+    }
+};
